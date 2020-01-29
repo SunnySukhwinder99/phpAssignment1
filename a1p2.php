70871dd7663264b60eaada3845f5d84a971716b4
Sunny Sukhwinder <Ssukhwindersunn2458@conestogac.on.ca>
2020-01-29 14:43:50 -0500

    second version

--- a1p2.php
+++ a1p2.php
@@ -22,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$newsletter = $_POST['newsletter'];
 		$age = $_POST['age'];
 
-		echo "<p> First Name: $firsname<br> Last Name: $lastname <br> Age: $age <br> Newsletter: $newsletter </p>";
+		echo "<p> First Name: $firstname<br> Last Name: $lastname <br> Age: $age <br> Newsletter: $newsletter </p>";
 
 
 	}
@@ -50,11 +50,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 	<fieldset><legend>Account Creation:</legend>
 
-	<p><label>First Name: <input type="text" name="firstname" size="20" maxlength="40"></label></p>
+	<p><label>First Name: <input type="text" name="firstname" size="20" maxlength="40" value=<?php if(isset($_POST['firstname'])) echo $_POST['firstname']; ?>></label></p>
 	
-	<p><label>Last Name: <input type="text" name="lastname" size="20" maxlength="40"></label></p>
+	<p><label>Last Name: <input type="text" name="lastname" size="20" maxlength="40" value=<?php if(isset($_POST['firstname'])) echo $_POST['lastname']; ?>></label></p>
 
-	<p><label>Age: <input type="text" name="age" size="20" maxlength="40"></label></p>
+	<p><label>Age: <input type="text" name="age" size="20" maxlength="40" value=<?php if(isset($_POST['firstname'])) echo $_POST['age']; ?>></label></p>
 	
 	<p><label for="newsletter">Subscribe to newsletter: </label><input type="radio" name="newsletter" value="Y"> Yes <input type="radio" name="newsletter" value="N"> No</p>
 
